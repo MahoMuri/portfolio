f18a395caaf2b9d08f2cb9acd232ad74e06158ac
MahoMuri <paolo.santos_12@yahoo.com.ph>
2023-02-15 07:12:19 +0800

    fix: removed unused dependency from main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,7 @@ import App from "./App.vue";
 import "./assets/css/tailwind.scss";
 import { router } from "./router";
 import { inject } from "@vercel/analytics";
-import VueScrollTo from "vue-scrollto";
 import "iconify-icon";
 
 inject();
-createApp(App).use(router).use(VueScrollTo).mount("#app");
+createApp(App).use(router).mount("#app");
